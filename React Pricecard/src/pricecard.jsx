3e6f29ac25fb33c04ebd927544572671cdb9df28
Selvakumar Boominathan <selvans2k@gmail.com>
2024-03-30 23:55:13 +0530

    React structure added

---
+++ React Pricecard/src/pricecard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <div class='Details-container'>
+      <h6>Free</h6>
+      <h1>$0/month</h1>
+      <hr />
+      <ul>
+        <li>✅ Single User</li>
+        <li>✅ 50GB Storage</li>
+        <li>✅ Unlimited Public Projects</li>
+        <li>✅ Community Access</li>
+        <li>❌ Unlimited Private Projects</li>
+        <li>❌ Dedicated Phone Support</li>
+        <li>❌ Free Subdomain</li>
+        <li>❌ Monthly Status Reports</li>
+      </ul>
+      <button>BUTTON</button>
+
+    </div>
+  )
+
+}
+
+export default Card;
