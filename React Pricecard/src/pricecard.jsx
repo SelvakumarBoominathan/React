63b380922d89188d4f63da2c685f4c59c2b311af
Selvakumar Boominathan <selvans2k@gmail.com>
2024-03-31 01:13:43 +0530

    new cards and styles were added

--- React Pricecard/src/pricecard.jsx
+++ React Pricecard/src/pricecard.jsx
@@ -2,25 +2,62 @@ import React from 'react';
 
 function Card() {
   return (
-    <div class='Details-container'>
-      <h6>Free</h6>
-      <h1>$0/month</h1>
-      <hr />
-      <ul>
-        <li>✅ Single User</li>
-        <li>✅ 50GB Storage</li>
-        <li>✅ Unlimited Public Projects</li>
-        <li>✅ Community Access</li>
-        <li>❌ Unlimited Private Projects</li>
-        <li>❌ Dedicated Phone Support</li>
-        <li>❌ Free Subdomain</li>
-        <li>❌ Monthly Status Reports</li>
-      </ul>
-      <button>BUTTON</button>
 
+    <div class="parent-container">
+      <div id='Details-container1'>
+        <h6>Free</h6>
+        <h1>$0/month</h1>
+        <hr />
+        <ul>
+          <li>✅ Single User</li>
+          <li>✅ 50GB Storage</li>
+          <li>✅ Unlimited Public Projects</li>
+          <li>✅ Community Access</li>
+          <li>❌ Unlimited Private Projects</li>
+          <li>❌ Dedicated Phone Support</li>
+          <li>❌ Free Subdomain</li>
+          <li>❌ Monthly Status Reports</li>
+        </ul>
+        <button>BUTTON</button>
+      </div>
+
+      <div id='Details-container2'>
+        <h6>PLUS</h6>
+        <h1>$9/month</h1>
+        <hr />
+        <ul>
+          <li>✅ 5 Users</li>
+          <li>✅ 50GB Storage</li>
+          <li>✅ Unlimited Public Projects</li>
+          <li>✅ Community Access</li>
+          <li>✅ Unlimited Private Projects</li>
+          <li>✅ Dedicated Phone Support</li>
+          <li>✅ Free Subdomain</li>
+          <li>❌ Monthly Status Reports</li>
+        </ul>
+        <button>BUTTON</button>
+      </div>
+
+      <div id='Details-container3'>
+        <h6>PRO</h6>
+        <h1>$49/month</h1>
+        <hr />
+        <ul>
+          <li>✅ Unlimited Users</li>
+          <li>✅ 50GB Storage</li>
+          <li>✅ Unlimited Public Projects</li>
+          <li>✅ Community Access</li>
+          <li>✅ Unlimited Private Projects</li>
+          <li>✅ Dedicated Phone Support</li>
+          <li>✅ Free Subdomain</li>
+          <li>✅ Monthly Status Reports</li>
+        </ul>
+        <button>BUTTON</button>
+      </div>
     </div>
   )
 
 }
 
 export default Card;
+
